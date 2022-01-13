2b3cb3ee4cf2cc873e582a57dc73b6f94f75eef8
hmzel <le.lonely.alex@gmail.com>
2022-01-13 17:56:37 -0600

    added names to Perks enum

--- src/main/java/me/zelha/thepit/zelenums/Perks.java
+++ src/main/java/me/zelha/thepit/zelenums/Perks.java
@@ -1,36 +1,46 @@
 package me.zelha.thepit.zelenums;
 
+import java.util.List;
+
 public enum Perks {
-    GOLDEN_HEADS,
-    FISHING_ROD,
-    LAVA_BUCKET,
-    STRENGTH_CHAINING,
-    SAFETY_FIRST,
-    BARBARIAN,
-    MINEMAN,
-    INSURANCE,
-    TRICKLE_DOWN,
-    LUCKY_DIAMOND,
-    SPAMMER,
-    BOUNTY_HUNTER,
-    STREAKER,
-    ASSISTANT_STREAKER,
-    CO_OP_CAT,
-    CONGLOMERATE,
-    GLADIATOR,
-    VAMPIRE,
-    RECON,
-    OVERHEAL,
-    RAMBO,
-    OLYMPUS,
-    DIRTY,
-    FIRST_STRIKE,
-    SOUP,
-    MARATHON,
-    THICK,
-    KUNG_FU_KNOWLEDGE
+    GOLDEN_HEADS("Golden Heads"),
+    FISHING_ROD("Fishing Rod"),
+    LAVA_BUCKET("Lava Bucket"),
+    STRENGTH_CHAINING("Strength-Chaining"),
+    SAFETY_FIRST("Safety First"),
+    BARBARIAN("Barbarian"),
+    MINEMAN("Mineman"),
+    INSURANCE("Insurance"),
+    TRICKLE_DOWN("Trickle-down"),
+    LUCKY_DIAMOND("Lucky Diamond"),
+    SPAMMER("Spammer"),
+    BOUNTY_HUNTER("Bounty Hunter"),
+    STREAKER("Streaker"),
+    ASSISTANT_STREAKER("Assistant Streaker"),
+    CO_OP_CAT("Co-op Cat"),
+    CONGLOMERATE("Conglomerate"),
+    GLADIATOR("Gladiator"),
+    VAMPIRE("Vampire"),
+    RECON("Recon"),
+    OVERHEAL("Overheal"),
+    RAMBO("Rambo"),
+    OLYMPUS("Olympus"),
+    DIRTY("Dirty"),
+    FIRST_STRIKE("First Strike"),
+    SOUP("Soup"),
+    MARATHON("Marathon"),
+    THICK("Thick"),
+    KUNG_FU_KNOWLEDGE("Kung Fu Knowledge");
 
+    private final String name;
 
+    Perks(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
 
 
