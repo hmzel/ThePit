c59ef4f97c54c793b77a88970092f8bd5f34c711
hmzel <le.lonely.alex@gmail.com>
2022-01-13 17:50:41 -0600

    added enum Perks

---
+++ src/main/java/me/zelha/thepit/zelenums/Perks.java
@@ -0,0 +1,56 @@
+package me.zelha.thepit.zelenums;
+
+public enum Perks {
+    GOLDEN_HEADS,
+    FISHING_ROD,
+    LAVA_BUCKET,
+    STRENGTH_CHAINING,
+    SAFETY_FIRST,
+    BARBARIAN,
+    MINEMAN,
+    INSURANCE,
+    TRICKLE_DOWN,
+    LUCKY_DIAMOND,
+    SPAMMER,
+    BOUNTY_HUNTER,
+    STREAKER,
+    ASSISTANT_STREAKER,
+    CO_OP_CAT,
+    CONGLOMERATE,
+    GLADIATOR,
+    VAMPIRE,
+    RECON,
+    OVERHEAL,
+    RAMBO,
+    OLYMPUS,
+    DIRTY,
+    FIRST_STRIKE,
+    SOUP,
+    MARATHON,
+    THICK,
+    KUNG_FU_KNOWLEDGE
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
