0b10090317498147c1de34f2bea0cc76eabeb1ca
hmzel <le.lonely.alex@gmail.com>
2022-01-13 21:44:18 -0600

    added names (not colored) to Passives enum

--- src/main/java/me/zelha/thepit/zelenums/Passives.java
+++ src/main/java/me/zelha/thepit/zelenums/Passives.java
@@ -1,24 +1,30 @@
 package me.zelha.thepit.zelenums;
 
 public enum Passives {
-    XP_BOOST("passive_xp_boost", "§bXP Boost"),
-    GOLD_BOOST("passive_gold_boost", "§6Gold Boost"),
-    MELEE_DAMAGE("passive_melee_damage", "§cMelee Damage"),
-    BOW_DAMAGE("passive_bow_damage", "§7Bow Damage"),
-    DAMAGE_REDUCTION("passive_damage_reduction", "§9Damage Reduction"),
-    BUILD_BATTLER("passive_build_battler", "§aBuild Battler"),
-    EL_GATO("passive_el_gato", "§dEl Gato");
+    XP_BOOST("passive_xp_boost", "XP Boost", "§bXP Boost"),
+    GOLD_BOOST("passive_gold_boost", "Gold Boost", "§6Gold Boost"),
+    MELEE_DAMAGE("passive_melee_damage", "Melee Damage", "§cMelee Damage"),
+    BOW_DAMAGE("passive_bow_damage", "Bow Damage", "§7Bow Damage"),
+    DAMAGE_REDUCTION("passive_damage_reduction", "Damage Reduction", "§9Damage Reduction"),
+    BUILD_BATTLER("passive_build_battler", "Build Battler", "§aBuild Battler"),
+    EL_GATO("passive_el_gato", "El Gato", "§dEl Gato");
 
     private final String id;
+    private final String name;
     private final String colorfulName;
 
-    Passives(String id, String colorfulName) {
+    Passives(String id, String name, String colorfulName) {
         this.id = id;
+        this.name = name;
         this.colorfulName = colorfulName;
     }
 
     public String getID() {
         return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getColorfulName() {
