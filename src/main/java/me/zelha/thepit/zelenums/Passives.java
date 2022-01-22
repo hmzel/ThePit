3e086ff142f768e05cada64b41e9e9a6bb41b4ea
hmzel <le.lonely.alex@gmail.com>
2022-01-21 20:04:04 -0600

    greatly simplified determineLevelRequirement
    and made it not static
    
    and changed its name to getLevelRequirement

--- src/main/java/me/zelha/thepit/zelenums/Passives.java
+++ src/main/java/me/zelha/thepit/zelenums/Passives.java
@@ -5,25 +5,27 @@ import me.zelha.thepit.mainpkg.data.PlayerData;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-public enum Passives {
-    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE, 10),
-    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE, 20),
-    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE, 30),
-    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE, 30),
-    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE, 30),
-    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL, 40),
-    EL_GATO("El Gato", "§dEl Gato", Material.CAKE, 50);
+public enum Passives {//0 tierMultipler is just a placeholder dont hurt me please
+    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE, 10, 0),
+    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE, 20, 15),
+    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE, 30, 0),
+    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE, 30, 0),
+    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE, 30, 0),
+    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL, 40, 0),
+    EL_GATO("El Gato", "§dEl Gato", Material.CAKE, 50, 0);
 
     private final String name;
     private final String colorfulName;
     private final Material material;
     private final int baseLevelReq;
+    private final int tierMultiplier;
 
-    Passives(String name, String colorfulName, Material material, int baseLevelReq) {
+    Passives(String name, String colorfulName, Material material, int baseLevelReq, int tierMultiplier) {
         this.name = name;
         this.colorfulName = colorfulName;
         this.material = material;
         this.baseLevelReq = baseLevelReq;
+        this.tierMultiplier = tierMultiplier;
     }
 
     public String getName() {
@@ -50,34 +52,10 @@ public enum Passives {
         return result;
     }
 
-    public static int determineLevelRequirement(Passives passive, Player p) {//most levels unknown atm
-        PlayerData pData = Main.getInstance().getPlayerData(p);//0 is a placeholder dont yell at me please
-        int level = 0;
+    public int getLevelRequirement(Player p) {
+        PlayerData pData = Main.getInstance().getPlayerData(p);
 
-        switch (passive) {
-            case XP_BOOST:
-                level = XP_BOOST.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-            case GOLD_BOOST://t2 is 35, just going off that, idk the actual levels
-                level = GOLD_BOOST.baseLevelReq + (15 * pData.getPassiveTier(passive));
-                break;
-            case MELEE_DAMAGE:
-                level = MELEE_DAMAGE.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-            case BOW_DAMAGE:
-                level = BOW_DAMAGE.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-            case DAMAGE_REDUCTION:
-                level = DAMAGE_REDUCTION.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-            case BUILD_BATTLER:
-                level = BUILD_BATTLER.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-            case EL_GATO:
-                level = EL_GATO.baseLevelReq + (0 * pData.getPassiveTier(passive));
-                break;
-        }
-        return level;
+        return baseLevelReq + (tierMultiplier * pData.getPassiveTier(Passives.findByName(super.name())));
     }
 }
 
