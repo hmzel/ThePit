d694b8073db8f55cdf1ae38c9fd667dca46abfac
hmzel <le.lonely.alex@gmail.com>
2022-01-21 19:43:23 -0600

    added baseLevelReq to Passives enum

--- src/main/java/me/zelha/thepit/zelenums/Passives.java
+++ src/main/java/me/zelha/thepit/zelenums/Passives.java
@@ -3,22 +3,24 @@ package me.zelha.thepit.zelenums;
 import org.bukkit.Material;
 
 public enum Passives {
-    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE),
-    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE),
-    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE),
-    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE),
-    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE),
-    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL),
-    EL_GATO("El Gato", "§dEl Gato", Material.CAKE);
+    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE, 10),
+    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE, 20),
+    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE, 30),
+    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE, 30),
+    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE, 30),
+    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL, 40),
+    EL_GATO("El Gato", "§dEl Gato", Material.CAKE, 50);
 
     private final String name;
     private final String colorfulName;
     private final Material material;
+    private final int baseLevelReq;
 
-    Passives(String name, String colorfulName, Material material) {
+    Passives(String name, String colorfulName, Material material, int baseLevelReq) {
         this.name = name;
         this.colorfulName = colorfulName;
         this.material = material;
+        this.baseLevelReq = baseLevelReq;
     }
 
     public String getName() {
