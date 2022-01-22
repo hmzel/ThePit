060e48afc1d7126b24dedbba6a7f4a9c770c65b3
hmzel <le.lonely.alex@gmail.com>
2022-01-21 21:36:34 -0600

    added baseCost to Passives enum

--- src/main/java/me/zelha/thepit/zelenums/Passives.java
+++ src/main/java/me/zelha/thepit/zelenums/Passives.java
@@ -6,24 +6,26 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 public enum Passives {//0 tierMultipler is just a placeholder dont hurt me please
-    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE, 10, 0),
-    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE, 20, 15),
-    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE, 30, 0),
-    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE, 30, 0),
-    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE, 30, 0),
-    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL, 40, 0),
-    EL_GATO("El Gato", "§dEl Gato", Material.CAKE, 50, 0);
+    XP_BOOST("XP Boost", "§bXP Boost", Material.LIGHT_BLUE_DYE, 500, 10, 0),
+    GOLD_BOOST("Gold Boost", "§6Gold Boost", Material.ORANGE_DYE, 1000, 20, 15),
+    MELEE_DAMAGE("Melee Damage", "§cMelee Damage", Material.RED_DYE, 450, 30, 0),
+    BOW_DAMAGE("Bow Damage", "§eBow Damage", Material.YELLOW_DYE, 450, 30, 0),
+    DAMAGE_REDUCTION("Damage Reduction", "§9Damage Reduction", Material.CYAN_DYE, 450, 30, 0),
+    BUILD_BATTLER("Build Battler", "§aBuild Battler", Material.BONE_MEAL, 750, 40, 0),
+    EL_GATO("El Gato", "§dEl Gato", Material.CAKE, 1000, 50, 0);
 
     private final String name;
     private final String colorfulName;
     private final Material material;
+    private final int baseCost;
     private final int baseLevelReq;
     private final int levelTierMultiplier;
 
-    Passives(String name, String colorfulName, Material material, int baseLevelReq, int levelTierMultiplier) {
+    Passives(String name, String colorfulName, Material material, int baseCost, int baseLevelReq, int levelTierMultiplier) {
         this.name = name;
         this.colorfulName = colorfulName;
         this.material = material;
+        this.baseCost = baseCost;
         this.baseLevelReq = baseLevelReq;
         this.levelTierMultiplier = levelTierMultiplier;
     }
