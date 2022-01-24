468d4a2b2a4af1d26fa502b559ad18eef6ed8dd4
hmzel <le.lonely.alex@gmail.com>
2022-01-24 14:13:01 -0600

    fixed typo
    was using < instead of >= when i refactored the code and i didnt notice

--- src/main/java/me/zelha/thepit/upgrades/villager/UpgradesVillagerListener.java
+++ src/main/java/me/zelha/thepit/upgrades/villager/UpgradesVillagerListener.java
@@ -191,7 +191,7 @@ public class UpgradesVillagerListener implements Listener {//i hate this class
             p.sendMessage("§cYou are too low level to acquire this!");
             p.playSound(p.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return;
-        } else if (pData.getPassiveTier(passive) < 5) {
+        } else if (pData.getPassiveTier(passive) >= 5) {
             p.sendMessage("§aYou already unlocked the last upgrade!");
             p.playSound(p.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
             return;
