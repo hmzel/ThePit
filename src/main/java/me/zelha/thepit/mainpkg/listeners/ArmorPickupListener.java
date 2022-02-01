3fc06577114bb7dc0a4a08ba432731b0efab0e58
hmzel <le.lonely.alex@gmail.com>
2022-02-01 17:55:22 -0600

    added new listener ArmorPickupListener
    will be used for autoequipping armor on pickup

---
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/ArmorPickupListener.java
@@ -0,0 +1,28 @@
+package me.zelha.thepit.mainpkg.listeners;
+
+import org.bukkit.event.Listener;
+
+public class ArmorPickupListener implements Listener {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
