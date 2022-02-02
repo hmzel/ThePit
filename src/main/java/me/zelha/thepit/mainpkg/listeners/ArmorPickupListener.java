80b86feb55d378a37aedbcb7dc8cb89f5c071f3d
hmzel <le.lonely.alex@gmail.com>
2022-02-01 18:10:26 -0600

    updated ArmorPickupListener
    added arrays armorWeight0, armorWeight1, and armorWeight2, which will be used to determine whether or not to equip an armor piece over a currently equipped armor piece

--- src/main/java/me/zelha/thepit/mainpkg/listeners/ArmorPickupListener.java
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/ArmorPickupListener.java
@@ -1,9 +1,26 @@
 package me.zelha.thepit.mainpkg.listeners;
 
+import me.zelha.thepit.Main;
+import me.zelha.thepit.ZelLogic;
+import org.bukkit.Material;
 import org.bukkit.event.Listener;
+
+import static org.bukkit.Material.*;
 
 public class ArmorPickupListener implements Listener {
 
+    private final ZelLogic zl = Main.getInstance().getZelLogic();
+
+    private final Material[] armorWeight0 = {
+            CHAINMAIL_HELMET, CHAINMAIL_CHESTPLATE, CHAINMAIL_LEGGINGS, CHAINMAIL_BOOTS,
+            LEATHER_HELMET
+    };
+    private final Material[] armorWeight1 = {
+            IRON_HELMET, IRON_CHESTPLATE, IRON_LEGGINGS, IRON_BOOTS
+    };
+    private final Material[] armorWeight2 = {
+            DIAMOND_HELMET, DIAMOND_CHESTPLATE, DIAMOND_LEGGINGS, DIAMOND_BOOTS
+    };
 
 }
 
