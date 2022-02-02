fab950b33a82155ffd4f798c9142d69623d06620
hmzel <le.lonely.alex@gmail.com>
2022-02-01 18:13:16 -0600

    updated ArmorPickupListener
    added eventhandler onPickup which will set items, add items, etc

--- src/main/java/me/zelha/thepit/mainpkg/listeners/ArmorPickupListener.java
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/ArmorPickupListener.java
@@ -3,7 +3,12 @@ package me.zelha.thepit.mainpkg.listeners;
 import me.zelha.thepit.Main;
 import me.zelha.thepit.ZelLogic;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import static org.bukkit.Material.*;
 
@@ -22,6 +27,19 @@ public class ArmorPickupListener implements Listener {
             DIAMOND_HELMET, DIAMOND_CHESTPLATE, DIAMOND_LEGGINGS, DIAMOND_BOOTS
     };
 
+    @EventHandler
+    public void onPickup(EntityPickupItemEvent e) {
+
+        if (!zl.playerCheck(e.getEntity())) return;
+
+        Player p = (Player) e.getEntity();
+        PlayerInventory inv = p.getInventory();
+        ItemStack item = e.getItem().getItemStack();
+        Material type = item.getType();
+        String name = type.name();
+
+
+    }
 }
 
 
