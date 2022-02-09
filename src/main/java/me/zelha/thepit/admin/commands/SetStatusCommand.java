38c2c118114e5e08c0cb97884ee67bd343de733c
hmzel <le.lonely.alex@gmail.com>
2022-02-09 17:16:03 -0600

    updated SetStatusCommand
    refactored the code with my new style

--- src/main/java/me/zelha/thepit/admin/commands/SetStatusCommand.java
+++ src/main/java/me/zelha/thepit/admin/commands/SetStatusCommand.java
@@ -1,6 +1,7 @@
 package me.zelha.thepit.admin.commands;
 
 import me.zelha.thepit.Main;
+import me.zelha.thepit.ZelLogic;
 import me.zelha.thepit.mainpkg.data.PlayerData;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -10,22 +11,35 @@ import org.bukkit.entity.Player;
 
 public class SetStatusCommand implements CommandExecutor {
 
+    private final ZelLogic zl = Main.getInstance().getZelLogic();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player p2 = Bukkit.getPlayer(args[0]);
+        if (args.length == 0 || args.length > 2) {
+            sender.sendMessage("§5Wrong args. try: /setstatus nerd idling");
+            return true;
+        }
 
         if (args.length == 1) {
-            if (sender instanceof Player) {
-                Player p = (Player) sender;
-                PlayerData pData = Main.getInstance().getPlayerData(p);
-                pData.setStatus(args[0]);
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("you cant run this command like this from console are you *trying* to cause errors? lol");
+                return true;
             }
-        } else if (args.length == 2 && p2 != null && p2.isValid()) {
-            PlayerData pData2 = Main.getInstance().getPlayerData(p2);
 
-            pData2.setPrestige(Integer.parseInt(args[1]));
-        } else {
-            sender.sendMessage("§5Wrong args");
+            Player p = (Player) sender;
+            PlayerData pData = Main.getInstance().getPlayerData(p);
+            pData.setStatus(args[0]);
+        }
+
+        if (args.length == 2) {
+            if (!zl.playerCheck(Bukkit.getPlayer(args[0]))) {
+                sender.sendMessage("§5Offline players are currently not supported.");
+                return true;
+            }
+
+            Player p2 = Bukkit.getPlayer(args[0]);
+            PlayerData pData2 = Main.getInstance().getPlayerData(p2);
+            pData2.setStatus(args[1]);
         }
 
         return true;
