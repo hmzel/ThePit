0318e329e3fbe5bc50989d9fa7ee1c1e97a8dd9c
hmzel <le.lonely.alex@gmail.com>
2022-02-09 17:08:34 -0600

    updated SetPerkCommand
    refactored the code with my new style

--- src/main/java/me/zelha/thepit/admin/commands/SetPerkCommand.java
+++ src/main/java/me/zelha/thepit/admin/commands/SetPerkCommand.java
@@ -1,6 +1,7 @@
 package me.zelha.thepit.admin.commands;
 
 import me.zelha.thepit.Main;
+import me.zelha.thepit.ZelLogic;
 import me.zelha.thepit.mainpkg.data.PlayerData;
 import me.zelha.thepit.zelenums.Perks;
 import org.bukkit.Bukkit;
@@ -11,58 +12,62 @@ import org.bukkit.entity.Player;
 
 public class SetPerkCommand implements CommandExecutor {
 
+    private final ZelLogic zl = Main.getInstance().getZelLogic();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
+        if (args.length <= 1 || args.length > 3) {
+            sender.sendMessage("§5Wrong args. try: /setperk nerd 1 unset");
+            return true;
+        }
+
+        if (args.length == 2) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("you cant run this command like this from console are you *trying* to cause errors? lol");
+                return true;
+            } else if (Perks.findByEnumName(args[1]) == null) {
+                sender.sendMessage("§5Invalid command usage. \n§5try /setperk 1 unset");
+                return true;
+            }
+
             Player p = (Player) sender;
             PlayerData pData = Main.getInstance().getPlayerData(p);
 
-            if (args.length == 2) {
-                if (Perks.findByEnumName(args[1]) != null) {
-                    switch (args[0]) {
-                        case "1":
-                        case "2":
-                        case "3":
-                        case "4":
-                            pData.setPerkAtSlot(Integer.parseInt(args[0]), Perks.findByEnumName(args[1]));
-                            break;
-                        default:
-                            p.sendMessage("§5Invalid command usage." +
-                                    "\n§5try /setperk 1 unset");
-                            break;
-                    }
-                } else {
-                    p.sendMessage("§5Invalid command usage." +
-                            "\n§5try /setperk 1 unset");
-                }
-            } else if (args.length == 3) {
-                if (Bukkit.getPlayer(args[0]) != null) {
-                    Player p2 = Bukkit.getPlayer(args[0]);
-                    PlayerData pData2 = Main.getInstance().getPlayerData(p2);
+            switch (args[0]) {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    pData.setPerkAtSlot(Integer.parseInt(args[0]), Perks.findByEnumName(args[1]));
+                    break;
+                default:
+                    p.sendMessage("§5Invalid command usage. \n§5try /setperk 1 unset");
+                    break;
+            }
+        }
 
-                    if (Perks.findByEnumName(args[2]) != null) {
-                        switch (args[1]) {
-                            case "1":
-                            case "2":
-                            case "3":
-                            case "4":
-                                pData2.setPerkAtSlot(Integer.parseInt(args[1]), Perks.findByEnumName(args[2]));
-                                break;
-                            default:
-                                p.sendMessage("§5Invalid command usage." +
-                                        "\n§5try /setperk name 1 unset");
-                                break;
-                        }
-                    } else {
-                        p.sendMessage("§5Invalid command usage." +
-                                "\n§5try /setperk name 1 unset");
-                    }
-                } else {
-                    p.sendMessage("§5i haven't added support for offline players yet");
-                }
-            } else {
-                p.sendMessage("§5Invalid command usage." +
-                        "\n§5try /setperk 1 unset");
+        if (args.length == 3) {
+            if (!zl.playerCheck(Bukkit.getPlayer(args[0]))) {
+                sender.sendMessage("§5Offline players are currently not supported.");
+                return true;
+            }  else if (Perks.findByEnumName(args[2]) == null) {
+                sender.sendMessage("§5Invalid command usage. \n§5try /setperk nerd 1 unset");
+                return true;
+            }
+
+            Player p2 = Bukkit.getPlayer(args[0]);
+            PlayerData pData2 = Main.getInstance().getPlayerData(p2);
+
+            switch (args[1]) {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                    pData2.setPerkAtSlot(Integer.parseInt(args[1]), Perks.findByEnumName(args[2]));
+                    break;
+                default:
+                    sender.sendMessage("§5Invalid command usage. \n§5try /setperk nerd 1 unset");
+                    break;
             }
         }
         return true;
