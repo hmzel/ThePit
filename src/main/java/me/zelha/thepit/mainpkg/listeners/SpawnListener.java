6135a99368c7c5834669d5b4cceb301ab9889061
hmzel <le.lonely.alex@gmail.com>
2022-02-10 22:26:24 -0600

    updated SpawnListener
    refactored using new coding style

--- src/main/java/me/zelha/thepit/mainpkg/listeners/SpawnListener.java
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/SpawnListener.java
@@ -2,10 +2,7 @@ package me.zelha.thepit.mainpkg.listeners;
 
 import me.zelha.thepit.Main;
 import me.zelha.thepit.zelenums.Worlds;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -13,63 +10,41 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.util.BoundingBox;
 
-import static me.zelha.thepit.zelenums.Worlds.*;
+import static me.zelha.thepit.zelenums.Worlds.CASTLE;
+import static me.zelha.thepit.zelenums.Worlds.GENESIS;
 
 public class SpawnListener implements Listener {
-    private final BoundingBox elementalsSpawn = BoundingBox.of(new Location(Bukkit.getWorld("Elementals"), 0.0, 110.0, 0.0), 25.0, 15.0, 25.0);
-    private final BoundingBox coralsSpawn = BoundingBox.of(new Location(Bukkit.getWorld("Corals"), 0.0, 110.0, 0.0), 25.0, 15.0, 25.0);
-    private final BoundingBox seasonsSpawn = BoundingBox.of(new Location(Bukkit.getWorld("Seasons"), 0.0, 110.0, 0.0), 25.0, 15.0, 25.0);
-    private final BoundingBox castleSpawn = BoundingBox.of(new Location(Bukkit.getWorld("Castle"), 0.0, 105.0, 0.0), 25.0, 15.0, 25.0);
-    private final BoundingBox genesisSpawn = BoundingBox.of(new Location(Bukkit.getWorld("Genesis"), 0.0, 90.0, 0.0), 25.0, 15.0, 25.0);
 
     public boolean spawnCheck(Location location) {
         Worlds world = Worlds.findByName(location.getWorld().getName());
         double x = location.getX();
         double y = location.getY();
         double z = location.getZ();
+        double spawnY;
 
-        if (world == ELEMENTALS && elementalsSpawn.contains(x, y, z)) {
-            return true;
+        if (world == GENESIS) {
+            spawnY = 90.0;
+        } else if (world == CASTLE) {
+            spawnY = 105.0;
+        } else {
+            spawnY = 110.0;
         }
-        if (world == CORALS && coralsSpawn.contains(x, y, z)) {
-            return true;
-        }
-        if (world == SEASONS && seasonsSpawn.contains(x, y, z)) {
-            return true;
-        }
-        if (world == CASTLE && castleSpawn.contains(x, y, z)) {
-            return true;
-        }
-        if (world == GENESIS && genesisSpawn.contains(x, y, z)) {
-            return true;
-        }
-
-        return false;
+        return BoundingBox.of(new Location(location.getWorld(), 0.0, spawnY, 0.0), 25.0, 15.0, 25.0).contains(x, y, z);
     }
 
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
-        if (spawnCheck(e.getEntity().getLocation())) {
-            e.setCancelled(true);
-        }
+        if (spawnCheck(e.getEntity().getLocation())) e.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent e) {
-        if (spawnCheck(e.getBlock().getLocation())) {
-            if (!Main.getInstance().blockPriviledges.contains(e.getPlayer())) {
-                e.setCancelled(true);
-            }
-        }
+        if (spawnCheck(e.getBlock().getLocation()) && !Main.getInstance().blockPriviledges.contains(e.getPlayer())) e.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent e) {
-        if (spawnCheck(e.getBlock().getLocation())) {
-            if (!Main.getInstance().blockPriviledges.contains(e.getPlayer())) {
-                e.setCancelled(true);
-            }
-        }
+        if (spawnCheck(e.getBlock().getLocation()) && !Main.getInstance().blockPriviledges.contains(e.getPlayer())) e.setCancelled(true);
     }
 }
 
