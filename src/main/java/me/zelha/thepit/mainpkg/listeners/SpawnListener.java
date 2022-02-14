406019141db5efb69b16f5e8ed0025f6a9d75639
hmzel <le.lonely.alex@gmail.com>
2022-02-14 17:43:46 -0600

    made it so your streak is reset if you enter spawn without using /spawn or dying

--- src/main/java/me/zelha/thepit/mainpkg/listeners/SpawnListener.java
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/SpawnListener.java
@@ -2,15 +2,40 @@ package me.zelha.thepit.mainpkg.listeners;
 
 import me.zelha.thepit.Main;
 import me.zelha.thepit.ZelLogic;
+import me.zelha.thepit.mainpkg.data.PlayerData;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class SpawnListener implements Listener {
 
     private final ZelLogic zl = Main.getInstance().getZelLogic();
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        new BukkitRunnable() {
+            boolean spamPrevention = true;
+
+            @Override
+            public void run() {
+                if (!zl.playerCheck(e.getPlayer())) cancel();
+
+                Player p = e.getPlayer();
+                PlayerData pData = Main.getInstance().getPlayerData(p);
+                spamPrevention = zl.spawnCheck(e.getPlayer().getLocation());
+
+                if (spamPrevention && pData.getStreak() != 0) {
+                    pData.setStreak(0);
+                    e.getPlayer().sendMessage("§c§lRESET! §7streak reset for standing in the spawn area!");
+                }
+            }
+        }.runTaskTimer(Main.getInstance(), 0, 1);
+    }
 
     @EventHandler
     public void onDamage(EntityDamageEvent e) {
