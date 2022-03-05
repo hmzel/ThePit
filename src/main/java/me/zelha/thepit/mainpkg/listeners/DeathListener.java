830d3c770d7e50dc4e2c27c241d93ecfa659d06b
hmzel <le.lonely.alex@gmail.com>
2022-03-05 14:49:38 -0600

    fixed typo

--- src/main/java/me/zelha/thepit/mainpkg/listeners/DeathListener.java
+++ src/main/java/me/zelha/thepit/mainpkg/listeners/DeathListener.java
@@ -185,7 +185,7 @@ public class DeathListener implements Listener {
             Player damager = Main.getInstance().getAssistUtils().getLastDamager(p);
 
             if (damager != null) {
-                p.spigot().sendMessage(new ComponentBuilder("§c§lDEATH! by " + zl.getColorBracketAndLevel(damager.getUniqueId().toString()) + " §7" + damager.getName() + " §e§lVIEW RECAP")
+                p.spigot().sendMessage(new ComponentBuilder("§c§lDEATH! §7by " + zl.getColorBracketAndLevel(damager.getUniqueId().toString()) + " §7" + damager.getName() + " §e§lVIEW RECAP")
                         .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§eClick to view kill recap!")))
                         .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/killrecap " + p.getUniqueId()))
                         .create());
