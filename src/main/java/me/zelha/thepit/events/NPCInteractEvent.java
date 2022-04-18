a685973d33d65f9810ffd22728d1620a2d4c9126
hmzel <le.lonely.alex@gmail.com>
2022-04-18 18:00:54 -0500

    added NPCInteractEvent to clean up some code later

---
+++ src/main/java/me/zelha/thepit/events/NPCInteractEvent.java
@@ -0,0 +1,35 @@
+package me.zelha.thepit.events;
+
+import me.zelha.thepit.zelenums.NPCs;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class NPCInteractEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final NPCs npc;
+    private final Player player;
+
+    public NPCInteractEvent(Player player, NPCs npc) {
+        this.player = player;
+        this.npc = npc;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public NPCs getNPC() {
+        return npc;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+}
