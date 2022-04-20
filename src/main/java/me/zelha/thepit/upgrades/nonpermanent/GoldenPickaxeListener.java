137e4add4965871e9160d0018cac2fa317ba3919
hmzel <le.lonely.alex@gmail.com>
2022-04-19 20:05:08 -0500

    added sound and made it take 3 ticks between block poofs

--- src/main/java/me/zelha/thepit/upgrades/nonpermanent/GoldenPickaxeListener.java
+++ src/main/java/me/zelha/thepit/upgrades/nonpermanent/GoldenPickaxeListener.java
@@ -1,17 +1,20 @@
 package me.zelha.thepit.upgrades.nonpermanent;
 
 import me.zelha.thepit.Main;
-import me.zelha.thepit.utils.ZelLogic;
 import me.zelha.thepit.mainpkg.listeners.BlockListener;
+import me.zelha.thepit.utils.ZelLogic;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,24 +28,22 @@ public class GoldenPickaxeListener implements Listener {
 
     private final Map<UUID, Integer> hitCount = new HashMap<>();
 
-    private boolean isPlacedBlock(Block block) {
-        return BlockListener.placedBlocks.contains(block);
-    }
-
     @EventHandler
     public void onClick(PlayerInteractEvent e) {
-        if (e.getAction() != Action.LEFT_CLICK_BLOCK) return;
+        if (e.getAction() != Action.LEFT_CLICK_BLOCK && e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
         if (!zl.blockCheck(e.getClickedBlock())) return;
         if (e.getClickedBlock().getType() != OBSIDIAN) return;
-        if (!isPlacedBlock(e.getClickedBlock())) return;
+        if (!BlockListener.placedBlocks.contains(e.getClickedBlock())) return;
         if (!zl.itemCheck(e.getItem())) return;
         if (e.getItem().getType() != GOLDEN_PICKAXE) return;
 
         UUID uuid = e.getPlayer().getUniqueId();
         Block clicked = e.getClickedBlock();
+        Player p = e.getPlayer();
 
         hitCount.putIfAbsent(uuid, 0);
         hitCount.put(uuid, hitCount.get(uuid) + 1);
+        p.playSound(e.getPlayer().getLocation(), Sound.BLOCK_STONE_HIT, 1F, 1.5F);
 
         if (hitCount.get(uuid) >= 2) {
             World world = clicked.getWorld();
@@ -52,15 +53,28 @@ public class GoldenPickaxeListener implements Listener {
 
             hitCount.put(uuid, 0);
 
-            for (int i = 0; i < 5; i++) {
-                Block block = world.getBlockAt(new Location(world, x, y + i, z));
+            new BukkitRunnable() {
 
-                if (zl.blockCheck(block) && isPlacedBlock(block) && block.getType() == OBSIDIAN) {
-                    block.setType(AIR);
-                    block.getWorld().spawnParticle(Particle.CLOUD, block.getLocation(), 5, 0.5, 0.5, 0.5, 0);
-                    BlockListener.placedBlocks.remove(block);
+                private int i = 0;
+
+                @Override
+                public void run() {
+                    Block block = world.getBlockAt(new Location(world, x, y + i, z));
+
+                    if (zl.blockCheck(block) && BlockListener.placedBlocks.contains(block) && block.getType() == OBSIDIAN) {
+                        block.setType(AIR);
+                        block.getWorld().spawnParticle(Particle.CLOUD, block.getLocation(), 5, 0.5, 0.5, 0.5, 0);
+                        BlockListener.placedBlocks.remove(block);
+                        p.playSound(p.getLocation(), Sound.BLOCK_STONE_HIT, 1F, 2F);
+                    } else {
+                        cancel();
+                    }
+
+                    i++;
+
+                    if (i == 5) cancel();
                 }
-            }
+            }.runTaskTimer(Main.getInstance(), 3, 3);
         }
     }
 
