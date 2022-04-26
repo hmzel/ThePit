6410c68a0cbde3c2124fd54286c98084334e904f
hmzel <le.lonely.alex@gmail.com>
2022-04-25 21:41:30 -0500

    fixed a potential bug in PitDamageEvent and added Arrow variable + getter

--- src/main/java/me/zelha/thepit/events/PitDamageEvent.java
+++ src/main/java/me/zelha/thepit/events/PitDamageEvent.java
@@ -1,5 +1,9 @@
 package me.zelha.thepit.events;
 
+import me.zelha.thepit.Main;
+import me.zelha.thepit.utils.ZelLogic;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
@@ -7,21 +11,36 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PitDamageEvent extends Event implements Cancellable {
 
     private static final HandlerList HANDLERS = new HandlerList();
+    private final ZelLogic zl = Main.getInstance().getZelLogic();
     private final Player damaged;
     private final Player damager;
     private final Map<EntityDamageEvent.DamageModifier, Double> modifiers;
     private boolean cancelled = false;
     private double damage;
+    private final Arrow arrow;
 
     public PitDamageEvent(EntityDamageByEntityEvent event) {
-        this.damaged = (Player) event.getEntity();
-        this.damager = (Player) event.getDamager();
+        Entity damagerEntity = event.getDamager();
+        Player damaged = (Player) event.getEntity();
+        Player damager;
+
+        if (damagerEntity instanceof Arrow) {
+            damager = (Player) ((Arrow) damagerEntity).getShooter();
+            this.arrow = (Arrow) damagerEntity;
+        } else {
+            damager = (Player) damagerEntity;
+            this.arrow = null;
+        }
+
+        this.damaged = damaged;
+        this.damager = damager;
         this.damage = event.getFinalDamage();
         this.modifiers = new HashMap<>();
 
@@ -63,6 +82,11 @@ public class PitDamageEvent extends Event implements Cancellable {
 
     public double getDamage(EntityDamageEvent.DamageModifier modifier) {
         return modifiers.get(modifier);
+    }
+
+    @Nullable
+    public Arrow getArrow() {
+        return arrow;
     }
 
     public void setDamage(double damage) {
