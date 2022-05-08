ef00884a8aba6c6c91be1730b74cdfc8c2eb8415
hmzel <le.lonely.alex@gmail.com>
2022-05-07 22:29:42 -0500

    added explicious

---
+++ src/main/java/me/zelha/thepit/upgrades/permanent/ministreaks/ExpliciousMinistreak.java
@@ -0,0 +1,15 @@
+package me.zelha.thepit.upgrades.permanent.ministreaks;
+
+import me.zelha.thepit.Main;
+import me.zelha.thepit.mainpkg.data.PlayerData;
+import org.bukkit.entity.Player;
+
+public class ExpliciousMinistreak extends Ministreak {
+
+    @Override
+    public void onTrigger(Player player) {
+        PlayerData pData = Main.getInstance().getPlayerData(player);
+
+        pData.setExp(pData.getExp() - 12);
+    }
+}
