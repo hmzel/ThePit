813b15d76b4db8d84578e9f1e20e99c627351237
hmzel <le.lonely.alex@gmail.com>
2022-05-07 21:16:55 -0500

    added Ministreak parent class

---
+++ src/main/java/me/zelha/thepit/upgrades/permanent/ministreaks/Ministreak.java
@@ -0,0 +1,30 @@
+package me.zelha.thepit.upgrades.permanent.ministreaks;
+
+import me.zelha.thepit.Main;
+import me.zelha.thepit.events.PitDamageEvent;
+import me.zelha.thepit.utils.ZelLogic;
+import org.bukkit.entity.Player;
+
+public class Ministreak {
+
+    protected final ZelLogic zl = Main.getInstance().getZelLogic();
+
+    public void onTrigger(Player player) {
+    }
+
+    public double getDamagedModifier(Player damaged, PitDamageEvent event) {
+        return 0;
+    }
+
+    public double getDamagerModifier(Player damager, PitDamageEvent event) {
+        return 0;
+    }
+
+    public double getEXPModifier(Player player) {
+        return 1;
+    }
+
+    public double getGoldModifier(Player player) {
+        return 1;
+    }
+}
